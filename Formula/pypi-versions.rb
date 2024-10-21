0a1824a99e9ddd8723df1a245b3cc71dfd2eedc7
Dencyuman <takotakocyuman@gmail.com>
2024-10-22 04:14:31 +0900

    fix

--- Formula/pypi-versions.rb
+++ Formula/pypi-versions.rb
@@ -1,7 +1,7 @@
 class PypiVersions < Formula
   desc "CLI tool to fetch package versions and metadata from PyPI."
   homepage "https://github.com/Dencyuman/ppv"
-  url "https://github.com/Dencyuman/pypi-versions/archive/refs/tags/v1.1.10.tar.gz"
+  url "https://github.com/Dencyuman/pypi-versions/archive/refs/tags/v1.1.11.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
