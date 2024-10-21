9add9c70d498238ae4b8ff603dee5798f7013fbc
Dencyuman <takotakocyuman@gmail.com>
2024-10-22 03:26:49 +0900

    fix formula file

--- Formula/pypi-versions.rb
+++ Formula/pypi-versions.rb
@@ -1,19 +1,19 @@
-# Formula/pypi-versions.rb
-class PypiVersions < Formula
-  desc "PyPI Versions is a CLI tool to fetch package versions from PyPI."
-  homepage "https://github.com/Dencyuman/pypi-versions"
-  url "https://github.com/Dencyuman/pypi-versions/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  license "MIT"
+# Formula/ppv.rb
+class Ppv < Formula
+  desc "CLI tool to fetch package versions and metadata from PyPI."
+  homepage "https://github.com/Dencyuman/ppv"
+  url "https://github.com/Dencyuman/ppv/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "5fcd5055074d22c3d23a756bb3aaab1abc6851585393feaf1489cb84ee9c2f4d"
+  license "MIT" # プロジェクトのライセンスに応じて変更
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", "ppv"
-    bin.install "ppv"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
-    assert_match "Available versions for", shell_output("#{bin}/ppv requests 2>&1", 0)
+    # バージョン確認のテスト
+    assert_match "ppv version 1.1.0", shell_output("#{bin}/ppv --version")
   end
 end
