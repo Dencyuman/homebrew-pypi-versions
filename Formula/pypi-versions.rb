d32281bd546ae789435fe48ddd0cd95fd99a3b7e
Dencyuman <takotakocyuman@gmail.com>
2024-10-22 03:42:17 +0900

    fix

--- Formula/pypi-versions.rb
+++ Formula/pypi-versions.rb
@@ -1,10 +1,9 @@
-# Formula/ppv.rb
-class Ppv < Formula
+class PypiVersions < Formula
   desc "CLI tool to fetch package versions and metadata from PyPI."
   homepage "https://github.com/Dencyuman/ppv"
   url "https://github.com/Dencyuman/ppv/archive/refs/tags/v1.1.2.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  license "MIT" # プロジェクトのライセンスに応じて変更
+  license "MIT"
 
   depends_on "go" => :build
 
