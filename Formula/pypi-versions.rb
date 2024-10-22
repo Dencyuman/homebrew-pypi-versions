b1cb566b51bbf220039ebc309ee361487685c3f9
Dencyuman <takotakocyuman@gmail.com>
2024-10-22 19:11:40 +0900

    fix formula

--- Formula/pypi-versions.rb
+++ Formula/pypi-versions.rb
@@ -3,7 +3,7 @@ class PypiVersions < Formula
   desc "PyPI Versions is a CLI tool to fetch package versions from PyPI."
   homepage "https://github.com/Dencyuman/pypi-versions"
   url "https://github.com/Dencyuman/pypi-versions/archive/refs/tags/v1.1.14.tar.gz"
-  sha256 "fbe37224c5016346b91fd0bfa17e17e23fb446b65405822019c1835cb0e7fac1"
+  sha256 "5f7d330eeb9bbcfffa175dae542447724ba2e4741dae95bf05ee68a79452aea6"
   license "MIT"
 
   depends_on "go" => :build
